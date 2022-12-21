9fdbd7084144d54cb5a52c2ced930d67cf1b4544
Mitpf <113504721+Mitpf@users.noreply.github.com>
2022-12-21 18:56:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,24 +23,3 @@ function navigator(event) {
     }    
 }
 
-/*
-//s.js
-
-const objAlls = {};
-
-Array.from(document.querySelector('#stuff').children).forEach(s => {
-    objAlls[s.id]=s
-});
-
-
-document.querySelector('#stuff').replaceChildren();
-
-const main = document.querySelector('#main-view');
-
-//export
-function showSec(id) {
-
-    const element = objAlls[id]
-    main.replaceChildren(element);
-}
-*/
